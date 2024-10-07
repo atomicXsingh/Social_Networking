4b15e082daf3391ec44e2fcae6d98f963860dc59
atomicXsingh <149986363+atomicXsingh@users.noreply.github.com>
2024-10-07 18:38:49 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header';
+import Doubt from './components/doubt_sharing';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Doubt></Doubt>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
